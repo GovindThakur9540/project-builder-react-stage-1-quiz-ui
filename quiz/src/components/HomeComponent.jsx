480e6a465b020a5b55ab6806a94b87cc25a613e3
Govind Thakur <govindthakur9540@gmail.com>
2021-03-23 15:06:34 +0530

    first Commit

---
+++ quiz/src/components/HomeComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class HomeComponent extends Component{
+    render(){
+        return(
+            <div className="home-container">
+                <h1>Quiz App</h1>
+                <button>Play</button>
+            </div>
+        );
+    }
+}
+
+export default HomeComponent;
---
+++ quiz/src/components/ResultComponent.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+export default class ResultComponent extends Component{
+    render(){
+        return(
+            <div className="result-main-container">
+                <i className = "check-circle" aria-hidden="true"></i>
+                <h1>Result</h1>
+                <div className="result-container">
+                    <h3>You need more practice!</h3>
+                    <h2>Your Score: 20%</h2>
+                    <p>Total number of question:<span>15</span></p>
+                    <p>Number of attempted question:<span>9</span></p>
+                    <p>Number of Correct Answers:<span>3</span></p>
+                    <p>Number of Wrong Answers:<span>6</span></p>
+                </div>
+                <div className="button-container">
+                    <button>Play Again</button>
+                    <button>Back to Home</button>
+                </div>
+            </div>
+        )
+    }
+}
